b66e8e9b8521e1b543fa81f9975a2190116f7b7c
lekaiser <sylvanus010@gmail.com>
2020-10-31 17:25:21 +0100

    Begin writting converting method

--- converter.py
+++ converter.py
@@ -1,1 +1,8 @@
 # Convert the html tables to csv file format
+import pandas
+
+def convert_to_csv(**tables):
+    for table_key, table_value in tables.items():
+        list_of_tables = table_value
+        for value in list_of_tables:
+            table = value
