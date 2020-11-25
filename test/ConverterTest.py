fd6dc4fd7aaa6717f6f4e3667c8c84bcf745f4e4
ange-clarisse <angecla16@gmail.com>
2020-11-25 19:02:27 +0100

    add test_get_number_of_tables_per_page,test_check_nb_columns,test_check_nb_rows

--- test/ConverterTest.py
+++ test/ConverterTest.py
@@ -19,6 +19,29 @@ class ConverterTest(unittest.TestCase):
     def test_number_tables(self):
         self.assertEqual(8, extractor.get_number_of_tables_on_page(self.url1), "Number of tables isn't correct")
 
+    def test_check_nb_columns(self):
+        urls = {"Comparison_of_Afrikaans_and_Dutch":"https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"}
+        table = extractor.get_html_tables(**urls)
+        dict = {"Comparison_of_Afrikaans_and_Dutch":table}
+        file = converter.convert_to_csv(**dict)
+
+        nbColumnTable = extractor.get_number_of_columns_in_the_table(table)
+        nbColumnFile = extractor.get_number_of_columns_in_the_csv(file)
+
+        self.assertEqual(nbColumnTable,nbColumnFile)
+
+    def test_check_nb_rows(self):
+        urls = {"Comparison_of_Afrikaans_and_Dutch":"https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"}
+        table = extractor.get_html_tables(**urls)
+        dict = {"Comparison_of_Afrikaans_and_Dutch":table}
+        file = converter.convert_to_csv(**dict)
+
+        nbRowsTable = extractor.get_number_of_rows_in_the_table(table)
+        nbRowsFile = extractor.get_number_of_rows_in_the_csv(file)
+
+        self.assertEqual(nbRowsTable,nbRowsFile)
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- test/HtmlTablesExtractorTest.py
+++ test/HtmlTablesExtractorTest.py
@@ -1,15 +1,25 @@
 import unittest
 import htmlTablesExtractor as extractor
 
+
 class HtmlTablesExtractorTest(unittest.TestCase):
-    def __init__(self):
-        super().__init__()
-        self.urls = extractor.get_urls()
-        self.tables = extractor.get_html_tables(**self.urls)
+    #def __init__(self):
+        #super().__init__()
+        #self.urls = extractor.get_urls()
+        #self.tables = extractor.get_html_tables(**self.urls)
 
-    def test_urls(self):
-        self.assertEqual(True, False)
+    #def test_urls(self):
+        #self.assertEqual(True, False)
 
+    def test_get_number_of_tables_per_page(self):
+        urls = {"Comparison_of_Afrikaans_and_Dutch":"https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"}
+        nb_tables = extractor.get_number_of_tables_per_page(**urls)
+        self.assertEqual({"Comparison_of_Afrikaans_and_Dutch":3},nb_tables,"c'est bel et bien le nombre de tables qui se trouve sur cette page")
+
+        #url = ["https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"]
+        #value_list = [val for (k, val) in urls.items() if nb_tables == 4]
+        #self.assertIsNot(val,extractor.__url_state(url))
+        #self.assertRaises(ValueError,extractor.get_number_of_tables_per_page(**urls),4)
 
 if __name__ == '__main__':
     unittest.main()
