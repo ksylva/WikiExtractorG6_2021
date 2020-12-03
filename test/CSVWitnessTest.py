c69048399c949267f3a9bbdea7be87e18511e0dd
ange-clarisse <angecla16@gmail.com>
2020-12-03 16:57:07 +0100

    modification in test_witness_two

--- test/CSVWitnessTest.py
+++ test/CSVWitnessTest.py
@@ -3,11 +3,12 @@ import unittest
 class CSVWitnessTest(unittest.TestCase):
 
     url_two = "https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"
-    converted_file_two = "Comparison_of_Afrikaans_and_Dutch_1.csv"
-    wanted_file_two = "Comparison_of_Afrikaans_and_Dutch_1.csv"
+    converted_file_two = "Comparison_of_Afrikaans_and_Dutch_3.csv"
+    wanted_file_two = "Comparison_of_Afrikaans_and_Dutch_3.csv"
 
     ground_truth_path = "../groundTruth/"
     extracted_path = "../output/"
+
     url_four = "https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch"
     converted_file_four = "Comparison_of_Afrikaans_and_Dutch_2.csv"
     wanted_file_four = "Comparison_of_Afrikaans_and_Dutch_2.csv"
@@ -21,8 +22,7 @@ class CSVWitnessTest(unittest.TestCase):
     """Case : Comparison of a predefined hand-made CSV of a given table with the generated CSV of the same table
       Result : Generated CSV correspond to hand-made one"""
     def test_witness_four(self):
-
-        result = self.compare_csv_files(self.url_four, self.wanted_file_four, self.converted_file_four)
+        result = self.compare_csv_files(self.wanted_file_four, self.converted_file_four)
         self.assertTrue(result, "Generated file does not correspond expected file")
 
     # def test_witness_three(self):
